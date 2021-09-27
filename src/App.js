dd30173336f2822ed32330802040c51ab2f29b5c
Katie Li <katie01px2019@sasstudent.org>
2021-09-27 12:59:43 -0500

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
 
 const Banner = ({ title }) => (
   <h1>{ title }</h1>
@@ -30,6 +31,7 @@ const Course = ({ course }) => (
   </div>
 );
 
+
 const schedule = {
   "title": "CS Courses for 2018-2019",
   "courses": {
@@ -56,11 +58,31 @@ const schedule = {
   }
 };
 
-const App = () =>  (
-  <div className="container">
-    <Banner title={ schedule.title } />
-    <CourseList courses={ schedule.courses } />
-  </div>
-);
+
+const App = () => {
+  const [schedule, setSchedule] = useState();
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, []);
+
+  if (!schedule) return <h1>Loading schedule...</h1>;
+
+  return (
+    <div className="container">
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </div>
+  );
+};
+
+
 
 export default App;
